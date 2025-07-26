9d225929330c6f387e4df147ac738513d47879c5
沈東政 <bevis@innovue.ltd>
2025-07-26 19:46:03 +0800

    fix: update BASE URL to point to the correct repository path

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const BASE = "https://raw.githubusercontent.com/JacobLinCool/Myself-BBS-API/data/";
+export const BASE = "https://raw.githubusercontent.com/vanisoul/Myself-BBS-API/refs/heads/main/data/";
 export const TTL = 300;
 export const CACHE_CONTROL = `max-age=${TTL}, s-maxage=${TTL}, public`;
